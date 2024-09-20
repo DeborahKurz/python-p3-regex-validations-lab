868e07fa40f0ecebaeef421e6a0429d56021fe0f
Deborah Kurz <DeborahKurz6@gmail.com>
2024-09-20 17:17:31 -0600

    Phone_number and email_a ddress completed

--- lib/regex.py
+++ lib/regex.py
@@ -6,8 +6,8 @@ import re
 name = r"[A-Z][a-zA-Z]*(?:[-' ][A-z]+)*"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"\d{10}|\(\d{3}\) \d{3}-\d{4}|\d{3}-\d{3}-\d{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[a-zA-Z][a-zA-Z0-9.@]*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"
 email_regex = re.compile(email_address)
